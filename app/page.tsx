3b64a9094d9596e208861deb78ec985ac73944df
Amartya Sinha <amartyasinha2002@gmail.com>
2023-12-28 00:39:12 +0530

    responsive sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,156 @@
-import Image from 'next/image'
+"use client"
+import React, { useState } from "react";
+
+const NavbarTop = ({ toggleLeftSidebar, toggleRightSidebar, toggleMobileLeftSidebar, toggleMobileRightSidebar }) => (
+  <div className="bg-red-300 h-12 w-full fixed top-0 ">
+    <div className="flex justify-between mx-6 h-full ">
+      <button onClick={toggleMobileLeftSidebar} className="md:hidden">Left Mobile Sidebar</button>
+      <button onClick={toggleMobileRightSidebar} className="md:hidden">Right Mobile Sidebar</button>
+      <button className="hidden md:block" onClick={toggleLeftSidebar} >Left Sidebar toggle</button>
+      <button className="hidden md:block" onClick={toggleRightSidebar}>Right Sidebar toggle</button>
+
+    </div>
+  </div>
+);
+
+const LeftSidebar = ({ isOpen }) => (
+  <aside className={`min-w-[15rem] lg:min-w-[20rem] bg-pink-200 rounded-2xl h-screen hidden  ${isOpen ? ' md:block' : ' hidden '}  `}>
+    <div className="h-screen flex justify-center">
+      <SidebarBox />
+    </div>
+  </aside>
+);
+
+
+const RightSidebar = ({ isOpen }) => (
+  <aside className={`min-w-[12rem]   bg-pink-200 rounded-2xl h-screen hidden ${isOpen ? 'md:block' : 'hidden'} `}>
+    <div className="h-screen flex justify-center">
+      <LeftSidebarBox />
+    </div>
+  </aside>
+);
+
+const MiddleSection = () => (
+  <section className="w-[40rem] bg-orange-200 rounded-2xl h-[300vh] ">
+  </section>
+)
+
+const SidebarBox = () => (
+  <div className="bg-red-300 h-72  min-w-[15rem] lg:w-[20rem] rounded-2xl fixed"></div>
+);
+
+
+const LeftSidebarBox = () => (
+  <div className="bg-red-400 h-72  w-[12rem] rounded-2xl fixed"></div>
+);
+
+const xMobileLeftSidebar = ({ isOpen, closeMobileLeftSidebar }) => (
+  <aside className={` w-full h-full bg-gray-200  fixed top-12 left-0 transform transition-transform ease-in-out duration-300 md:hidden ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+    <div className="w-64 h-full px-2 rounded bg-black text-white">
+
+      <div className="flex justify-end py-2">
+        <button onClick={closeMobileLeftSidebar} className="text-white focus:outline-none" >Close</button>
+      </div>
+
+      <div className="h-full border ">
+        {/* Sidebar content */}
+        <p>Hello, this is your sidebar content.</p>
+      </div>
+
+    </div>
+  </aside>
+);
+
+
+const MobileLeftSidebar = ({ isOpen, closeMobileLeftSidebar }) => (
+  <div className={`fixed top-12 inset-0 z-50 flex items-center justify-center backdrop-blur-sm transform transition-transform ease-in-out duration-300 md:hidden ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+    <aside className={`w-64 h-full  fixed  left-0  `}>
+
+      <div className="w-full h-full p-2 bg-black text-white ">
+
+    
+
+        <div className="h-[90%]  p-2">
+          {/* Sidebar content */}
+          <p className="text-gray-400">Hello, this is your Left sidebar content.</p>
+        </div>
+        <div className="flex justify-center ">
+          <button onClick={closeMobileLeftSidebar} className="text-gray-500 focus:outline-none">Close</button>
+        </div>
+
+      </div>
+    </aside>
+  </div>
+);
+
+const MobileRightSidebar = ({ isOpen, closeMobileLeftSidebar }) => (
+  <div className={`fixed top-12 inset-0 z-50 flex items-center justify-center backdrop-blur-sm transform transition-transform ease-in-out duration-300 md:hidden ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+    <aside className={`w-64 h-full  fixed  right-0  `}>
+
+      <div className="w-full h-full p-2 bg-black text-white ">
+
+    
+
+        <div className="h-[90%]  p-2">
+          {/* Sidebar content */}
+          <p className="text-gray-400">Hello, this is your Right sidebar content.</p>
+        </div>
+        <div className="flex justify-center ">
+          <button onClick={closeMobileLeftSidebar} className="text-gray-500 focus:outline-none">Close</button>
+        </div>
+
+      </div>
+    </aside>
+  </div>
+);
+
+
 
 export default function Home() {
+  const [isLeftSidebarOpen, setIsLeftSidebarOpen] = useState(true);
+  const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(true);
+  const [isMobileLeftSidebarOpen, setMobileLeftSidebarOpen] = useState(false);
+  const [isMobileRightSidebarOpen, setMobileRightSidebarOpen] = useState(false);
+  const toggleLeftSidebar = () => {
+    setIsLeftSidebarOpen(!isLeftSidebarOpen);
+  };
+
+  const toggleRightSidebar = () => {
+    setIsRightSidebarOpen(!isRightSidebarOpen);
+  };
+
+  const toggleMobileLeftSidebar = () => {
+    setMobileLeftSidebarOpen(!isMobileLeftSidebarOpen);
+  };
+  const closeMobileLeftSidebar = () => {
+    setMobileLeftSidebarOpen(!isMobileLeftSidebarOpen);
+  };
+  const toggleMobileRightSidebar = () => {
+    setMobileRightSidebarOpen(!isMobileRightSidebarOpen);
+  };
+  const closeMobileRightSidebar = () => {
+    setMobileRightSidebarOpen(!isMobileRightSidebarOpen);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <NavbarTop toggleLeftSidebar={toggleLeftSidebar} toggleRightSidebar={toggleRightSidebar} toggleMobileLeftSidebar={toggleMobileLeftSidebar}  toggleMobileRightSidebar={toggleMobileRightSidebar}/>
+
+      <MobileLeftSidebar isOpen={isMobileLeftSidebarOpen}
+        closeMobileLeftSidebar={closeMobileLeftSidebar} />
+
+<MobileRightSidebar isOpen={isMobileRightSidebarOpen}
+        closeMobileLeftSidebar={closeMobileRightSidebar} />
+
+      <main className="mt-20 mx-3 mb-6">
+
+        <div className="flex md:space-x-8 justify-center">
+
+          <LeftSidebar isOpen={isLeftSidebarOpen} />
+          <MiddleSection />
+          <RightSidebar isOpen={isRightSidebarOpen} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='fixed w-full bg-zinc-700'>
+      <div className='max-w-7xl mx-auto'>
+hello
+      </div>
+    </header>
+  )
+}
